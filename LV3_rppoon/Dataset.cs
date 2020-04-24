b8ed7f2f54ec835a585b99d4ffe0870b9b677dac
Mihaela Orić <oricmihaela@gmail.com>
2020-04-24 14:45:45 +0200

    LV3_rppoon

---
+++ LV3_rppoon/Dataset.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LV3_rppoon
+{
+    class Dataset
+    {
+        private List<List<string>> data; 
+        public Dataset()
+        {
+            this.data = new List<List<string>>();
+        }
+        public Dataset(string filePath) : this()
+        {
+            this.LoadDataFromCSV(filePath);
+        }
+        public void LoadDataFromCSV(string filePath)
+        {
+            using (System.IO.StreamReader reader = new System.IO.StreamReader(filePath))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    List<string> row = new List<string>();
+                    string[] items = line.Split(',');
+                    foreach (string item in items)
+                    {
+                        row.Add(item);
+                    }
+                    this.data.Add(row);
+                }
+            }
+        public Prototype Clone()
+        {
+            return (Prototype)this.MemberwiseClone();
+        }
+    }
+}
